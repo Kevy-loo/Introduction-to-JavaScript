ab2de89fa5926e5a355453cefaf262ce5aafa48c
kevin-liu <liu.kevin1997@gmail.com>
2021-11-02 17:53:56 -0400

    finished the assignment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ Do the following:
 */
 let votingAge = 19;
 if(votingAge >= 18){
-  console.log("true")
+  console.log(true)
 }
 
 
@@ -160,9 +160,29 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
+let computer = Math.random()
+if (computer <= 1/3){
+  computer = 'rock'
+}else if(computer <= 2/3){
+  computer = 'paper'
+}else if(computer <= 3/3){
+  computer = 'scissors'
+}
+
 
 function game(user, computer){
-  /*add your code here*/
+  
+  if (user === computer){
+    return `it's a tie`
+  }else if (user === 'scissors' && computer === 'paper'){
+    return `you win!`
+  }else if (user === 'paper' && computer === 'rock'){
+    return `you win!`
+  }else if (user === 'rock' && computer === 'scissors'){
+    return `you win!`
+  }else{
+    return `you lose!`
+  }
 }
 
 
@@ -178,8 +198,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  return kilometers * 0.621371;
 }
 
 
@@ -192,8 +212,9 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+   return cm / 30.48;
+  
 }
 
 
@@ -208,11 +229,13 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      for (let i = number; i > 0; i--){
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1} bottles of soda on the wall`;
+      }
 }
 
-
+console.log(annoyingSong(2))
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 //Grade Calculator
@@ -228,10 +251,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
-}
+function grade(score){
+  if(score >= 90){
+    return 'you got an A';
+  }else if (score >= 80){
+    return 'you got a B';
+  }else if (score >= 70){
+    return 'you got a C';
+  }else if (score >= 60){
+    return 'you got a D';
+  }else if (score < 60){
+    return 'you got an F';
 
+}
+}
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
